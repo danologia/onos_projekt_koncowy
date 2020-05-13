ff4b29faa0be8833ea75a877ea12f2df29f76c03
Szymon <wozniaks97@gmail.com>
2020-05-13 17:07:07 +0200

    add login and impact factor scraping

--- src/scrape_author_data.py
+++ src/scrape_author_data.py
@@ -16,19 +16,31 @@ from src.settings import NODES_DIR, AUTHORS_DIR
 UNKNOWN = 'Nieznane'
 
 
-def scrape_authors_data(authors):
+def scrape_authors_data(login, password, authors):
     options = Options()
     options.headless = True
     driver = webdriver.Firefox("./", options=options)
     pbar = tqdm(authors)
     all_entries = []
+    driver.get('https://dona.pwr.edu.pl/szukaj/')
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadButton_login")))
+    driver.find_element_by_css_selector("#RadButton_login").click()
+
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadTextBox_uzytkownik")))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadTextBox_haslo")))
+    driver.find_element_by_css_selector("#RadTextBox_uzytkownik").send_keys(login)
+    driver.find_element_by_css_selector("#RadTextBox_haslo").send_keys(password)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadButton_zaloguj")))
+    driver.find_element_by_css_selector("#RadButton_zaloguj").click()
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadDock_formularze_C_RadSearchBox1_Input")))
+
     for id, name in pbar:
         pbar.set_description(f"{id}: {name}")
         driver.get(f"https://dona.pwr.edu.pl/szukaj/default.aspx?nrewid={id}")
 
-        wait = WebDriverWait(driver, 20)
 
-        # %%
+        #%%
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadDock1_info_autora_C_RadLabel_pytanie1")))
         author_info = driver.find_element_by_css_selector("#RadDock1_info_autora_C_RadLabel_pytanie1").text
         data = author_info.split("\n")
@@ -64,6 +76,16 @@ def scrape_authors_data(authors):
 
         # %%
 
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadDock1_info_autora_C_RadLabel_liczba_prac")))
+        work_and_impact_factor_data = driver.find_element_by_css_selector(
+            "#RadDock1_info_autora_C_RadLabel_liczba_prac").text
+        tokens = work_and_impact_factor_data.split('\n')
+        if len(tokens) > 3:
+            impact_factor_line = tokens[3]
+            impact_factor = impact_factor_line.split(':')[1].strip()
+        else:
+            impact_factor = 0
+
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#RadTabStrip_dane_wyszukane")))
         button_row = driver.find_element_by_css_selector("#RadTabStrip_dane_wyszukane").text.split('\n')
         publications_number = '0'
@@ -90,6 +112,7 @@ def scrape_authors_data(authors):
             'disciplines': disciplines,
             'publications': publications_number,
             'citations': citations,
+            'impact_factor': impact_factor,
             'supervisorships': supervisorships
         }
         all_entries.append(entry)
@@ -97,8 +120,10 @@ def scrape_authors_data(authors):
 
 
 if __name__ == '__main__':
-    nodes = pd.read_csv(NODES_DIR)
-    authors_data = list(zip(nodes['id'].to_list(), nodes['label'].to_list()))
-    all_entries = scrape_authors_data(authors_data)
+    login = os.environ['LOGIN']
+    password = os.environ['PASSWORD']
+    nodes = pd.read_csv(NODES_DIR, dtype=object)
+    authors_data = list(zip(nodes['id'].to_list(), nodes['label'].to_list()))[:3]
+    all_entries = scrape_authors_data(login, password, authors_data)
     with open(AUTHORS_DIR, 'w') as fp:
         json.dump(all_entries, fp, indent=1, ensure_ascii=False)
